904ba1cd0c7aeb478749dae097754e0a3f936159
krestaino <kevinrestaino@gmail.com>
2019-12-14 23:44:44 -0600

    poc

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,192 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import classNames from 'classnames';
 
 export default class App extends Component {
   state = {
-    data: { message: null },
+    data: {
+      folders: [],
+      items: []
+    },
+    dataToPrint: {
+      username: true,
+      password: true,
+      totp: true,
+      notes: true,
+      fields: true
+    },
     error: null,
+    foldersToPrint: [],
+    lastModified: null,
     loading: true
   };
 
-  async fetch() {
-    try {
-      const { data } = await axios.get(process.env.REACT_APP_SERVER_HOST + '/api');
-      this.setState({ data });
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ loading: false });
-    }
+  getPrintItemLength = () => {
+    const { data, foldersToPrint } = this.state;
+
+    return data.items.filter(item => foldersToPrint.includes(item.folderId)).length;
+  };
+
+  formatDate(lastModified) {
+    const date = new Date(lastModified);
+    return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
   }
 
-  componentDidMount() {
-    this.fetch();
-  }
+  readFile = event => {
+    const reader = new FileReader();
+    const file = event.target.files[0];
+
+    reader.onload = event => {
+      const data = JSON.parse(event.target.result);
+      const foldersToPrint = data.folders.map(({ id }) => id);
+      const lastModified = this.formatDate(file.lastModified);
+
+      this.setState({ data, foldersToPrint, lastModified });
+    };
+    reader.readAsText(file);
+  };
+
+  toggleDataOption = option => {
+    const dataToPrint = { ...this.state.dataToPrint };
+    dataToPrint[option] = !dataToPrint[option];
+    this.setState({ dataToPrint });
+  };
+
+  toggleFolder = id => {
+    const foldersToPrint = [...this.state.foldersToPrint];
+    const index = foldersToPrint.indexOf(id);
+
+    foldersToPrint.includes(id) ? foldersToPrint.splice(index, 1) : foldersToPrint.push(id);
+
+    this.setState({ foldersToPrint });
+  };
 
   render() {
-    const { data, error, loading } = this.state;
+    const { dataToPrint, foldersToPrint, data, lastModified } = this.state;
 
     return (
-      <main className="bg-gray-900 text-white flex flex-col h-screen items-center justify-center">
-        {loading ? 'Loading…' : data.message || error.toString()}
+      <main>
+        <div className="bg-gray-900 no-print">
+          <div className="container mx-auto px-4 py-8 text-gray-100">
+            <input onChange={this.readFile} type="file" />
+            {data.folders.length > 0 && (
+              <div>
+                <h2>Data</h2>
+                <ul className="flex">
+                  {Object.keys(dataToPrint).map(option => (
+                    <li key={option}>
+                      <input
+                        name={option}
+                        type="checkbox"
+                        checked={dataToPrint[option]}
+                        onChange={() => this.toggleDataOption(option)}
+                      />
+                      <span className="capitalize">{option}</span>
+                    </li>
+                  ))}
+                </ul>
+
+                <h2>Folders</h2>
+                <ul className="flex">
+                  {data.folders.map(({ id, name }) => (
+                    <li key={id}>
+                      <input
+                        name={id}
+                        type="checkbox"
+                        checked={foldersToPrint.includes(id)}
+                        onChange={() => this.toggleFolder(id)}
+                      />
+                      <span>{name}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+        <section className="container mx-auto px-4 my-8 text-gray-700">
+          {data.items.length > 0 && (
+            <div>
+              <div className="px-4">
+                <div className="flex justify-between items-baseline">
+                  <h1 className="font-bold text-2xl text-gray-900">Bitwarden Vault</h1>
+                  <span>Logins – {this.getPrintItemLength()}</span>
+                </div>
+                <div className="flex justify-between items-baseline">
+                  <span>Data – Username, Password, TOTP</span>
+                  <span>{lastModified}</span>
+                </div>
+              </div>
+              <ul>
+                {data.items.map(({ fields, folderId, login, name, notes }, index) => (
+                  <li
+                    className={classNames({
+                      hidden: !foldersToPrint.includes(folderId),
+                      'bg-gray-200 border border-gray-400 rounded mt-4 break-words': true
+                    })}
+                    key={index}
+                  >
+                    <div className="px-4 py-2 font-bold">{name}</div>
+                    {login && (
+                      <div>
+                        {login.username && (
+                          <div
+                            className={classNames({
+                              hidden: !dataToPrint['username'],
+                              'px-4 py-2 border-t border-gray-400': true
+                            })}
+                          >
+                            <span className="text-gray-500">U – </span>
+                            <span>{login.username}</span>
+                          </div>
+                        )}
+                        {login.password && (
+                          <div
+                            className={classNames({
+                              hidden: !dataToPrint['password'],
+                              'px-4 py-2 border-t border-gray-400': true
+                            })}
+                          >
+                            <span className="text-gray-500">P – </span>
+                            <span>{login.password}</span>
+                          </div>
+                        )}
+                        {login.totp && (
+                          <div
+                            className={classNames({
+                              hidden: !dataToPrint['totp'],
+                              'px-4 py-2 border-t border-gray-400': true
+                            })}
+                          >
+                            <span className="text-gray-500">T – </span>
+                            <span>{login.totp}</span>
+                          </div>
+                        )}
+                        {notes && (
+                          <div
+                            className={classNames({
+                              hidden: !dataToPrint['notes'],
+                              'px-4 py-2 border-t border-gray-400': true
+                            })}
+                          >
+                            <span className="text-gray-500">N – </span>
+                            <span>{notes}</span>
+                          </div>
+                        )}
+                        {fields &&
+                          fields.map((field, index) => (
+                            <div className="px-4 py-2 border-t border-gray-400" key={index}>
+                              <span className="text-gray-500">{field.name} – </span>
+                              <span>{field.value}</span>
+                            </div>
+                          ))}
+                      </div>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </section>
       </main>
     );
   }
--- server/src/index.js
+++ server/src/index.js
@@ -13,3 +13,28 @@ server.get('/api', async ({ res }) => {
 server.listen(SERVER_PORT, () => {
   console.info('Server: Accepting connections at ' + SERVER_HOST);
 });
+
+const fs = require('fs');
+
+const workFolderID = '11e8d6a8-984f-4625-9703-aa9100195fba';
+
+fs.readFile('data.json', (err, data) => {
+  if (err) throw err;
+
+  const formatted = [];
+
+  JSON.parse(data).items.filter(item => {
+    if (item.folderId !== workFolderID && item.type === 1) {
+      formatted.push({
+        name: item.name,
+        username: item.login ? item.login.username : null,
+        password: item.login ? item.login.password : null,
+        totp: item.login ? item.login.totp : null
+      });
+    }
+  });
+
+  fs.writeFile('format.json', JSON.stringify(formatted), 'utf8', function(err) {
+    if (err) throw err;
+  });
+});
